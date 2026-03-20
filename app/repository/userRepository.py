1eceb69ee7220081f98d3d395321f6c2fa1f48f5
syke-x <sujal.work.26@gmail.com>
2026-03-20 17:31:24 +0530

    feat : added create_table in userRepository class On Intialization of the application it will automatically create user table

--- app/repository/userRepository.py
+++ app/repository/userRepository.py
@@ -9,6 +9,21 @@ class UserRepository :
         if cls._instance is None :
             cls._instance = super().__new__(cls)
             cls._instance._conn = conn 
-            cls._instance.create_table()
+            cls._instance._create_table()
         return cls._instance
     
+
+    def _create_table():
+
+        self._conn.execute_query(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                user_id TEXT PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                hash_password TEXT NOT NULL,
+                created_at TIMESTAMP NOT NULL            
+            )
+            """
+        )
+        
