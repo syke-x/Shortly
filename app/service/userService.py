d9993e3022fe0fade017112eeac9b2a18562081e
syke-x <sujal.work.26@gmail.com>
2026-03-23 12:03:41 +0530

    feat : add create_user method in userService module create_user takes name , email and password and check if it's already present in the db if not then create a user in db and return.

--- app/service/userService.py
+++ app/service/userService.py
@@ -1,6 +1,8 @@
 from app.model.user import User 
 from app.repository.userRepository import UserRepository
 
+from datetime import datetime
+
 class UserService:
 
     _instance = None 
@@ -10,3 +12,25 @@ class UserService:
             cls._instance = super().__new__(cls)
             cls._instance._user_repo = user_repo
         return cls._instance
+
+
+    def create_user(self , name : str , email :str , password : str) -> User:
+
+        # check if the email is already present 
+        exisiting = _user_repo.find_by_email(email)
+
+        if exisiting:
+            raise ValueError("Email is already present")
+        
+        hash_password = AuthService.hash_password(password)
+
+        user = User(
+            user_id=utils.generate_user_id(),
+            name = name,
+            email=email,
+            hash_password=hash_password,
+            created_at=datetime.utc()
+        )
+
+        self._user_repo.save(user)
+        return user 
